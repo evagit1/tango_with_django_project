98ce0cff86ed24e50fdd0d6002f3709cbe350a14
Eva De Luca <3012530d@student.gla.ac.uk>
2026-02-06 16:13:30 +0000

    1 error ch8

--- rango/views.py
+++ rango/views.py
@@ -17,8 +17,10 @@ def index(request):
     return render(request, 'rango/index.html',context=context_dict)
 
 def about(request):
+   print(request.method)
+   print(request.user)
    # return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>"  )
-   return render(request,'rango/about.html')
+   return render(request,'rango/about.html',{})
 
 def show_category(request, category_name_slug):
     context_dict = {}
